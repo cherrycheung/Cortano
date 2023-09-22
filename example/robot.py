1fd7d25d40cad9f518ca42013e90d43f63dfb2d8
Timothy Yong <tyong_23@hotmail.com>
2023-09-22 15:58:51 -0700

    update lan for websocket

--- example/robot.py
+++ example/robot.py
@@ -1,10 +1,11 @@
 from cortano import RemoteInterface
 
 if __name__ == "__main__":
-  robot = RemoteInterface("1.2.3.4")
+  robot = RemoteInterface("192.168.1.1")
   while True:
     robot.update() # must never be forgotten
     color, depth, sensors = robot.read()
+    print(sensors)
 
     forward = robot.keys["w"] - robot.keys["s"]
     robot.motor[0] =  forward * 127
